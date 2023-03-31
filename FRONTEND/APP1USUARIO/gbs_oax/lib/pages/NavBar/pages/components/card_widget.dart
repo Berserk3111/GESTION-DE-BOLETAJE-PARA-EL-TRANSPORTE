7cd2738fa58e086c9fe37cc258d0b56c4e8b195b
Josue Martinez <52842324+zkrOzer@users.noreply.github.com>
2023-03-31 16:06:17 -0600

    Update card_widget.dart

--- FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/components/card_widget.dart
+++ FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/components/card_widget.dart
@@ -33,35 +33,31 @@ class _card_verticalState extends State<card_vertical> {
                     backgroundColor: Colors.grey.shade900,
                     backgroundImage: NetworkImage("https://cdn.forbes.com.mx/2020/02/Huatulco-Sectur-Oaxaca-1.jpg"),
                   ),
-                  const SizedBox(
-                    width: 300,
-                    height: 100,
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 10.0),
-                      child: Column(
-                        children: [
-                          Center(
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Column(
+                      children: [
+                        Center(
+                            child: Text(
+                          'Oaxaca',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                        Center(
+                            child: Text(
+                          'Huatulco',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.0),
+                          child: Center(
                               child: Text(
-                            'Oaxaca',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
+                            'Mas info',
+                            style: TextStyle(
+                                color: Color.fromRGBO(255, 94, 0, 1),
+                                fontSize: 20),
                           )),
-                          Center(
-                              child: Text(
-                            'Huatulco',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          )),
-                          Padding(
-                            padding: EdgeInsets.only(top: 20.0),
-                            child: Center(
-                                child: Text(
-                              'Mas info',
-                              style: TextStyle(
-                                  color: Color.fromRGBO(255, 94, 0, 1),
-                                  fontSize: 20),
-                            )),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
