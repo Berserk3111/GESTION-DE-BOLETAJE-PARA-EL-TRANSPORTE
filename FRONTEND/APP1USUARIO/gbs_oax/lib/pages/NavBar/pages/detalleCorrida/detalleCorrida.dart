b611b6b5a5d28e41e2174e31142f6ccf9d493509
Berserk3111 <75562908+Berserk3111@users.noreply.github.com>
2023-03-31 17:11:33 -0600

    CAMBIOS

---
+++ FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/detalleCorrida/detalleCorrida.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetalleCorrida extends StatefulWidget {
+  final corrida;
+  const DetalleCorrida({Key? key, required this.corrida}) : super(key: key);
+
+  @override
+  State<DetalleCorrida> createState() => _DetalleCorridaState();
+}
+
+class _DetalleCorridaState extends State<DetalleCorrida> {
+  @override
+  Widget build(BuildContext context) {
+    print(widget.corrida);
+    return Scaffold(
+      body: Center(child: Text(widget.corrida['ciudad_origen'])),
+    );
+  }
+}
