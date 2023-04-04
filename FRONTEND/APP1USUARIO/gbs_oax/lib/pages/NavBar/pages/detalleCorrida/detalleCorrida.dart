7e3c6218da1a0968ac2f80cd99f8c2b2edf71bae
Josue Martinez <52842324+zkrOzer@users.noreply.github.com>
2023-04-04 14:49:03 -0600

    Update detalleCorrida.dart

--- FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/detalleCorrida/detalleCorrida.dart
+++ FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/detalleCorrida/detalleCorrida.dart
@@ -17,107 +17,89 @@ class _DetalleCorridaState extends State<DetalleCorrida> {
   Widget build(BuildContext context) {
     print(widget.corrida);
     return Scaffold(
-      appBar: AppBar(
-          toolbarHeight: 80,
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Padding(
-            padding: const EdgeInsets.all(0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                const Text(
-                  "Detalles del Viaje",
-                  maxLines: 1,
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(0, 0, 0, 1)),
-                ),
-              ],
-            ),
-          )),
+      appBar: getAppBar(),
       body: getHome(),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                elevation: 10,
+              ),
+              child: Text('Comprar'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ComprarBoleto(corrida: widget.corrida)));
+              }),
+        ),
+      ),
+    );
+  }
+
+  AppBar getAppBar() {
+    return AppBar(
+      toolbarHeight: 150,
+      elevation: 10,
+      iconTheme: IconThemeData(color: Colors.white, size: 30, weight: 10),
+      flexibleSpace: Image.network(
+          "https://cdn.forbes.com.mx/2020/02/Huatulco-Sectur-Oaxaca-1.jpg",
+          fit: BoxFit.cover),
+      backgroundColor: Colors.white,
+      title: Text(
+        widget.corrida["ciudad_destino"] +
+            ", " +
+            widget.corrida["municipio_destino"],
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        maxLines: 5,
+      ),
     );
   }
 
   Widget getHome() {
-    return Container(
-        child: Card(
-      elevation: 5,
-      margin: EdgeInsets.all(15),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(50)),
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: FadeInImage.assetNetwork(
-                placeholder: '../assets/load.gif',
-                image:
-                    "https://cdn.forbes.com.mx/2020/02/Huatulco-Sectur-Oaxaca-1.jpg"),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 9.0),
-            child: Column(
-              children: [
-                Center(
-                    child: Text(
-                  widget.corrida['municipio_origen'],
-                  style: const TextStyle(
-                      color: Colors.orange,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                )),
-                Center(
-                    child: Text(
-                  widget.corrida['municipio_destino'],
-                  style: const TextStyle(
-                      color: Colors.orange,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                )),
-                Center(
-                    child: Text(
-                  ('Asientos totales: ') +
-                      widget.corrida['unidadModel']['asientos_totales']
-                          .toString(),
-                  style: const TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal),
-                )),
-                Center(
-                    child: Text(
-                  ('Precio: ') +
-                      numberFormat
-                          .format(widget.corrida['costosModel']['costo']),
-                  style: const TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
-                )),
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: Center(
-                      child: ElevatedButton(
-                    child: Text('Comprar'),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  ComprarBoleto(corrida: widget.corrida)));
-                    }
-                  )),
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              widget.corrida['municipio_origen'],
+              style: const TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
             ),
-          ),
-        ],
+            Text(
+              widget.corrida['municipio_destino'],
+              style: const TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              ('Asientos totales: ') +
+                  widget.corrida['unidadModel']['asientos_totales'].toString(),
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 15,
+                  fontWeight: FontWeight.normal),
+            ),
+            Text(
+              ('Precio: ') +
+                  numberFormat.format(widget.corrida['costosModel']['costo']),
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
