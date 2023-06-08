b8f050c58b0cd041187372929f424837c1be2e4b
Josue Martinez <52842324+zkrOzer@users.noreply.github.com>
2023-06-08 09:50:58 -0600

    Cambios en seleccionar asiento

--- FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/comprarBoleto/procesoBoleto.dart
+++ FRONTEND/APP1USUARIO/gbs_oax/lib/pages/NavBar/pages/comprarBoleto/procesoBoleto.dart
@@ -15,7 +15,7 @@ class SeatSelectionPage extends StatefulWidget {
 class _SeatSelectionPageState extends State<SeatSelectionPage> {
   List<int> selectedSeats = [];
 
-  Icon seat = Icon(
+  Icon seat = const Icon(
     Icons.event_seat,
     size: 100,
   );
@@ -25,10 +25,10 @@ class _SeatSelectionPageState extends State<SeatSelectionPage> {
   void selectSeat(index) {
     setState(() {
       if (selectedSeats.contains(index)) {
-        this.seat = Icon(Icons.airline_seat_legroom_normal);
+        this.seat = const Icon(Icons.airline_seat_legroom_normal);
         selectedSeats.remove(index);
       } else {
-        this.seat = Icon(Icons.event_seat);
+        this.seat = const Icon(Icons.event_seat);
         selectedSeats.add(index);
       }
     });
@@ -36,9 +36,9 @@ class _SeatSelectionPageState extends State<SeatSelectionPage> {
 
   Icon getIcon(index) {
     if (selectedSeats.contains(index)) {
-      return (Icon(Icons.event_seat_rounded));
+      return (const Icon(Icons.event_seat_rounded));
     } else {
-      return (Icon(Icons.event_seat));
+      return (const Icon(Icons.event_seat));
     }
   }
 
@@ -64,88 +64,82 @@ class _SeatSelectionPageState extends State<SeatSelectionPage> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(child: getHome()),
-          Text("Seleccione el Tipo de Asiento",
-              style: GoogleFonts.montserrat(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black)),
+          const SizedBox(
+            height: 20,
+          ),
           Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.child_care),
-                      color: Colors.green,
-                      onPressed: () {
-                        setState(() {
-                          print("niño");
-                          tipoPasaje = 1;
-                        });
-                      },
-                    ),
-                    Text("Niño",
-                        style: GoogleFonts.montserrat(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Colors.black))
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.person),
-                      color: Colors.blue,
-                      onPressed: () {
-                        setState(() {
-                          print("adulto");
-                          tipoPasaje = 2;
-                        });
-                      },
-                    ),
-                    Text("Adulto",
-                        style: GoogleFonts.montserrat(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Colors.black))
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.elderly),
-                      color: Colors.amber,
-                      onPressed: () {
-                        setState(() {
-                          print("tercera edad");
-                          tipoPasaje = 3;
-                        });
-                      },
-                    ),
-                    Text("Tercera Edad",
-                        style: GoogleFonts.montserrat(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Colors.black))
-                  ],
-                ),
-                Text("Asientos Totales: ${selectedSeats.length}",
-                    style: GoogleFonts.montserrat(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.orange))
-              ],
-            ),
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Seleccione el Tipo de Asiento",
+                style: GoogleFonts.montserrat(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.child_care),
+                    onPressed: () {
+                      setState(() {
+                        tipoPasaje = 1;
+                      });
+                    },
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        tipoPasaje = 2;
+                      });
+                    },
+                    icon: const Icon(Icons.person),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        tipoPasaje = 3;
+                      });
+                    },
+                    icon: const Icon(Icons.elderly),
+                  )
+                ],
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Niño"),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Adulto"),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Tercera Edad"),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Text("Asientos Totales: ${selectedSeats.length}",
+              style: GoogleFonts.montserrat(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.orange)),
+          Expanded(child: getHome()),
+          const SizedBox(
+            height: 100,
           )
         ],
       ),
@@ -180,7 +174,7 @@ class _SeatSelectionPageState extends State<SeatSelectionPage> {
             const SizedBox(height: 10);
           }
         },
-        child: Icon(Icons.arrow_forward),
+        child: const Icon(Icons.arrow_forward),
       ),
     );
   }
@@ -201,45 +195,48 @@ class _SeatSelectionPageState extends State<SeatSelectionPage> {
   }
 
   Widget getHome() {
-    return Container(
-      width: 250,
-      color: Colors.grey.shade700,
-      child: GridView.count(
-        mainAxisSpacing: BorderSide.strokeAlignCenter,
-        crossAxisSpacing: 2.5,
-        crossAxisCount: 4,
-        children: List.generate(20, (index) {
-          String seatNumber = 'Asiento ${index + 1}';
-          if (index != 0 &&
-              index != 1 &&
-              index != 7 &&
-              index != 6 &&
-              index != 10 &&
-              index != 14) {
-            return GestureDetector(
-                onTap: () {
-                  if (tipoPasaje == null) {
-                    print("selecciona tipo pasaje");
-                  } else {
-                    selectSeat(index);
-                  }
-                },
-                child: Container(
-                  width: 250,
-                  height: 100,
-                  margin: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: selectedSeats.contains(index)
-                        ? Colors.green
-                        : Colors.grey,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Center(child: getIcon(index)),
-                ));
-          } else {
-            return (Icon(Icons.block_outlined));
-          }
-        }),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 250,
+        color: Colors.transparent,
+        child: GridView.count(
+          mainAxisSpacing: BorderSide.strokeAlignCenter,
+          crossAxisSpacing: 2.5,
+          crossAxisCount: 4,
+          children: List.generate(20, (index) {
+            String seatNumber = 'Asiento ${index + 1}';
+            if (index != 0 &&
+                index != 1 &&
+                index != 7 &&
+                index != 6 &&
+                index != 10 &&
+                index != 14) {
+              return GestureDetector(
+                  onTap: () {
+                    if (tipoPasaje == null) {
+                      print("selecciona tipo pasaje");
+                    } else {
+                      selectSeat(index);
+                    }
+                  },
+                  child: Container(
+                    width: 250,
+                    height: 100,
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: selectedSeats.contains(index)
+                          ? Colors.green
+                          : Colors.grey,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(child: getIcon(index)),
+                  ));
+            } else {
+              return (const Icon(Icons.block_outlined));
+            }
+          }),
+        ),
       ),
     );
   }
